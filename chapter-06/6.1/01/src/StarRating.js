b379d5a0111db0b3b01cd027d10debea69f4964c
ohashi-tatsuki <bigbride428@gmail.com>
2021-12-27 11:07:12 +0900

    [update]starRatingを実装

--- chapter-06/6.1/01/src/StarRating.js
+++ chapter-06/6.1/01/src/StarRating.js
@@ -1,12 +1,31 @@
-import React from "react";
+// chapter-06を記載する 
+import React, { useState } from "react";
 import { FaStar } from "react-icons/fa";
 
-export default function StarRating() {
-  return [
-    <FaStar color="red" />,
-    <FaStar color="red" />,
-    <FaStar color="red" />,
-    <FaStar color="grey" />,
-    <FaStar color="grey" />
-  ];
+// f => f はダミー関数, undefined対策
+const Star = ({ selected = false, onSelect = f => f }) => (
+  // 1. スターアイコンをクリック, onSelectが発火
+  <FaStar color={selected ? "red" : "grey"} onClick={onSelect} />
+);
+
+export default function StarRating({ totalStars = 5 }) {
+  // const [State変数, 更新関数] = usestate(初期値);
+  // 3. stateの状態が変化したら再描画する
+  const [selectedStars, setSelectedStars] = useState(0);
+
+  return (
+    <>
+      {[...Array(totalStars)].map((n, i) => (
+        <Star
+         key={i}
+         selected={selectedStars > i}
+         // 2. 更新関数を=1, stateの状態が変わる
+         onSelect={() => setSelectedStars(i + 1)}
+        />
+      ))}
+      <p>
+        {selectedStars} of {totalStars} stars
+      </p>
+    </>
+  );
 }
--- chapter-06/6.1/02/src/StarRating.js
+++ chapter-06/6.1/02/src/StarRating.js
@@ -1,10 +1,10 @@
 import React from "react";
 import { FaStar } from "react-icons/fa";
 
-const Star = ({ selected = false }) => (
+const Star = ({ selected = false }) => {
   <FaStar color={selected ? "red" : "grey"} />
-);
+};
 
 export default function StarRating({ totalStars = 5 }) {
-  return [...Array(totalStars)].map((n, i) => <Star key={i} />);
-}
+  return [...Array(totalStars)].map((n, i) => <Star key={i} />)
+};
