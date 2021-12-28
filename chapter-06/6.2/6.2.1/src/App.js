f96e0563d7bf7642e989655f2fe1fae1ff22268b
ohashi-tatsuki <bigbride428@gmail.com>
2021-12-28 14:44:36 +0900

    [update]6.2.2 ユーザーの操作をコンポーネントツリーの下から上に伝えるを学習

--- chapter-06/6.2/6.2.1/src/App.js
+++ chapter-06/6.2/6.2.1/src/App.js
@@ -4,6 +4,21 @@ import ColorList from "./ColorList.js";
 
 export default function App() {
   // 更新関数のないuseState... 初期値出してねってこと？
-  const [colors] = useState(colorData);
-  return <ColorList colors={colors} />;
+  const [colors, setColors] = useState(colorData);
+  return (
+    <ColorList
+      colors={colors}
+      onRateColor={(id, rating) => {
+        const newColors = colors.map(color => 
+          color.id === id ? { ...color, rating } : color
+          );
+          setColors(newColors);
+      }}
+      onRemoveColor={id => {
+        // color.idにマッチしていないものは削除されている
+        const newColors = colors.filter(color => color.id !== id);
+        setColors(newColors);
+      }}
+    />
+  );
 }
--- chapter-06/6.2/6.2.1/src/ColorList.js
+++ chapter-06/6.2/6.2.1/src/ColorList.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import Color from "./Color";
 
-export default function ColorList({ colors = [] }) {
+export default function ColorList({ 
+  colors = [], 
+  onRemoveColor = f => f,
+  onRateColor = f => f 
+}) {
   // 配列の長さが0の時はテキストを表示する
-  if (!colors.length) return <div>No Color Listed.</div>;
+  if (!colors.length) return <div>No Color Listed. (Add a Color)</div>;
   return (
-    <div>
+    <div className='color-list'>
       {
-        colors.map(color => <Color key={color.id} {...color}/>)
+        colors.map(color => 
+          <Color 
+            key={color.id} 
+            {...color} 
+            onRemove={onRemoveColor}
+        />
+       )
       }
     </div>
   );
