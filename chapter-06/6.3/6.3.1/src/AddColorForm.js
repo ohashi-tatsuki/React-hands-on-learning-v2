28d269a85e9edbc11f6b31c1b118731c235d02fa
ohashi-tatsuki <bigbride428@gmail.com>
2022-01-02 11:12:32 +0900

    [update]6.3.1 refを使ったデータアクセスを学習

--- chapter-06/6.3/6.3.1/src/AddColorForm.js
+++ chapter-06/6.3/6.3.1/src/AddColorForm.js
@@ -1,20 +1,26 @@
-import React, { useRef } from "react";
+import React, { useRef } from 'react';
 
+// 制御されていないコンポーネント
 export default function AddColorForm({ onNewColor = f => f }) {
   const txtTitle = useRef();
   const hexColor = useRef();
 
   const submit = e => {
+    // クリック時にPOSTを送らない
     e.preventDefault();
+
+    // currentフィールドから入力値を取得
     const title = txtTitle.current.value;
     const color = hexColor.current.value;
+
     onNewColor(title, color);
     txtTitle.current.value = "";
     hexColor.current.value = "";
   };
 
   return (
-    <form onSubmit={submit}>
+    <form>
+      {/* useRefを参照 */}
       <input ref={txtTitle} type="text" placeholder="color title..." required />
       <input ref={hexColor} type="color" required />
       <button>ADD</button>
